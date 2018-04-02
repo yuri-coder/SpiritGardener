200a13430923947d65643a47fc21a228d5730c4e
yuri-coder <eirika.sawh@gmail.com>
2018-04-01 23:51:23 -0400

    Started trade classes
    + Created a TradeManager
    + Created a Trade class

---
+++ Assets/Scripts/Trade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trade {
+
+    public Dictionary<Item, int> itemsNeeded;
+    public Dictionary<Item, int> itemsReceived;
+    public string tradeName;
+
+    public Trade(Dictionary<Item, int> needed, Dictionary<Item, int> received, string name)
+    {
+        itemsNeeded = needed;
+        itemsReceived = received;
+        tradeName = name;
+    }
+
+    public Trade(Dictionary<Item, int> needed, Dictionary<Item, int> received)
+    {
+        itemsNeeded = needed;
+        itemsReceived = received;
+        tradeName = "";
+    }
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+}
---
+++ Assets/Scripts/TradeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TradeManager : MonoBehaviour {
+
+    public InventoryManager inventoryManager;
+
+    public List<Trade> availableTrades;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
