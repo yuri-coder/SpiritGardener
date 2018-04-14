9053686f083c70963a71a8a33e6d0144277bff42
yuri-coder <eirika.sawh@gmail.com>
2018-04-14 00:02:21 -0400

    Energy System
    + Created Player Manager to store player related mechanics
    + Began adding an energy system

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public int maxEnergy;
+    private int curEnergy;
+
+    void Awake()
+    {
+        
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RechargeEnergy(int amount)
+    {
+        curEnergy = (curEnergy + amount > maxEnergy) ? maxEnergy : curEnergy + amount;
+    }
+
+    public bool ConsumeEnergy(int amount)
+    {
+        if(curEnergy >= amount)
+        {
+            curEnergy -= amount;
+            return true;
+        }
+        else
+        {
+            DialogueManager.Instance.DisplayMessage("Not enough energy remaining!");
+            return false;
+        }
+    }
+
+    public int CurrentEnergy()
+    {
+        return curEnergy;
+    }
+
+}
