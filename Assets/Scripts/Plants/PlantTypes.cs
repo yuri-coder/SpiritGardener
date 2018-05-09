b8a185a7c6c80d30e76d1733bd4f5cb24d5a1f20
yuri-coder <eirika.sawh@gmail.com>
2018-05-08 20:59:29 -0400

    ItemTypes and PlantTypes
    + Added enums for PlantTypes and ItemTypes

---
+++ Assets/Scripts/Plants/PlantTypes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlantTypes {
+
+    Basic = 0,
+    Fire = 1,
+    Water = 2,
+    Light = 3,
+    Dark = 4,
+    Poison = 5
+
+}
